689b339daf83dd10e31352c649df352013722366
ChrisMattew <cristian.matteu@gmail.com>
2022-03-17 13:12:02 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,20 +5,15 @@ import './style.css';
 const formNode = document.getElementById('dynamic-form');
 const registerButton = document.getElementById('register-button');
 
-// registerButton.onclick = function onSubmit() {
-
-// };
-
-registerButton.addEventListener('click', (e) => {
-  e.preventDefault();
+registerButton.onclick = function onSubmit() {
   var data = {};
   var inputs = document.querySelectorAll('input');
   inputs.forEach((input) => {
     data[input.id] = input.value;
   });
-
-  alert(JSON.stringify(data));
-});
+  let o = Object.fromEntries(Object.entries(data).filter(([_, v]) => v != ''));
+  alert(JSON.stringify(o));
+};
 
 config.forEach((confItem) => {
   fieldsMap.section(confItem);
